d99b93def91ab193282be10f36aeb4bad709a4d8
adriankarlen <adrian.karlen@spp.se>
2024-11-23 00:48:03 +0100

    fix: add cwd path util

--- lua/buffed/utils.lua
+++ lua/buffed/utils.lua
@@ -1,11 +1,20 @@
 local H = {}
 
+---get basename for a filepath
 ---@param s string
 ---@return string
 H._basename = function(s)
-  return s:gsub("(.*[/\\])(.*)", "%2") or ""
+  return vim.fn.fnamemodify(s, ":t")
 end
 
+---get filepath with cwd as root for a filepath
+---@param s string
+---@return string
+H._cwd_filename = function (s)
+  return vim.fn.fnamemodify(s, ":~:.:h") .. "/" .. vim.fn.fnamemodify(s, ":t")
+end
+
+---apply hl to a string
 ---@param s string
 ---@param hl string
 ---@return string
@@ -13,18 +22,21 @@ H._colorize = function(s, hl)
   return "%#" .. hl .. "# " .. s
 end
 
+---add spacing with hl
 ---@param n number
 ---@return string
 H._spacer = function(n)
   local spaces = string.rep(" ", n)
-  return "%#TabLineSep#" .. spaces
+  return  H._colorize(spaces, "TabLineSep")
 end
 
+---adds alignment string
 ---@return string
 H._align = function()
   return "%="
 end
 
+---truncate end
 ---@return string
 H._truncate = function()
   return "%<"
