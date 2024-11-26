3a90d119a31c752430d2b5e758e34746de02ef74
adriankarlen <adrian.karlen@spp.se>
2024-11-26 13:36:28 +0100

    fix: improve autocmd events

--- lua/buffed/autocmds.lua
+++ lua/buffed/autocmds.lua
@@ -1,6 +1,6 @@
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd({ "BufWrite", "BufAdd", "InsertEnter", "InsertLeave" }, {
+autocmd({ "BufWrite", "BufAdd", "InsertCharPre", "InsertEnter","InsertLeave", "DiagnosticChanged" }, {
   callback = function()
     vim.opt.showtabline = 2
   end,
