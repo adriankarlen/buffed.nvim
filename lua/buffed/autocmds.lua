bca8be448269dd70f6783b8c1a115b17d91c9f60
adriankarlen <adrian.karlen@spp.se>
2024-11-29 07:55:27 +0100

    fix: dont react on current buffer events in autocmd

--- lua/buffed/autocmds.lua
+++ lua/buffed/autocmds.lua
@@ -1,6 +1,6 @@
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd({ "BufWrite", "BufAdd", "InsertCharPre", "InsertEnter","InsertLeave", "DiagnosticChanged" }, {
+autocmd({ "BufEnter" }, {
   callback = function()
     vim.opt.showtabline = 2
   end,
